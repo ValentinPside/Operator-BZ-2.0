8053e26cae63b1468336270e4dc7b666ca67c59d
ValentinPside <91324342+ValentinPside@users.noreply.github.com>
2025-02-22 18:09:47 +0300

    libs and start icon

---
+++ app/src/main/java/com/example/operatorbz2/app/App.kt
@@ -0,0 +1,16 @@
+package com.example.operatorbz2.app
+
+import android.app.Application
+import com.example.operatorbz2.di.AppComponent
+import com.example.operatorbz2.di.DaggerAppComponent
+
+class App : Application() {
+
+    val appComponent = DaggerAppComponent.factory().create(this)
+
+
+    companion object {
+        lateinit var appComponent: AppComponent
+            private set
+    }
+}
